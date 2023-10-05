7ea87a963a60a498fd043df918c6bc2b3c2285d7
Irene Couto <irenecoutto@gmail.com>
2023-10-05 15:18:13 -0300

    Adicionar arquivos de projeto.

---
+++ ads-backend-2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ads_backend_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
